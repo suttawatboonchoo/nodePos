608e6971baedbe80980868e8e449521c81ad5113
suttawat boonchoo <suttawat.boonchoo@gmail.com>
2024-06-17 22:20:16 +0700

    install mysql lib

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const mysql2 = require('mysql2');
+const mysql = mysql2.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'db_nodepos'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
